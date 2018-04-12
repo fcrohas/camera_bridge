90f3703df943052c2f7e70c0f25205daa9cda3fb
Fabrice Crohas <fabrice.crohas@itesoft.com>
2018-04-12 12:17:02 +0200

    Add webserver camera with /live url

--- app.js
+++ app.js
@@ -1,263 +1,45 @@
-const fs = require('fs');
-const Q = require('q');
-const axios = require("axios");
-const jsonic = require("jsonic");
-const CryptoJS = require("crypto-js");
+// HTTP server configuration
+const express = require('express')
+const app = express()
 
-const crypto = require("crypto");
-const mdh = require("./mdh.js");
-const mcodec = require("./mcodec.js");
+// Camera configuration
+const Camera = require('./mipc/camera');
 
-const IP_CAMERA = "http://192.168.2.73"
-const GENERATE_DH = "/ccm/cacs_dh_req.js?hfrom_handle=705790&dbnum_prime="+mdh.prime+"&droot_num="+mdh.g+"&dkey_a2b=%KEY_2AB%&dtid=0";
-const LOGIN = "/ccm/cacs_login_req.js?hfrom_handle=%FROM_HANDLE%&dlid=%LID%&dnid=%NID%&duser=%USERNAME%&dpass=%PASSWORD%&dsession_req=1&dparam__x_countz_=1&dparam=1&dparam_name=spv&dparam_value=v1";
-const LOGOUT = "/ccm/cacs_logout_req.js?hfrom_handle=%FROM_HANDLE%&hqid=%QID%&dnid=%NID%";
-const PIC_GET = "/ccm/ccm_pic_get.jpg?hfrom_handle=887330&dsess=1&dsess_nid=%NID%&dsess_sn=%USERNAME%&dtoken=p0_xxxxxxxxxx";
-const IMG_GET = "/ccm/ccm_img_get.js?hfrom_handle=%FROM_HANDLE%&dsess=1&dsess_nid=%NID%&dsess_sn=%USERNAME%&dtoken=vs0";
-const MMQ_CREATE = "/ccm/mmq_create.js?hfrom_handle=%FROM_HANDLE%&dtimeout=30000";
-const MMQ_PICK = "/ccm/mmq_pick.js?hfrom_handle=%FROM_HANDLE%&hqid=%QID%&dqid=%QID%&dtimeout=300000";
-const SUBSCRIBE = "/ccm/ccm_subscribe.js?hfrom_handle=%FROM_HANDLE%&hqid=%QID%&dsess=1&dsess_nid=%NID%";
-const INFO = "/ccm/ccm_info_get.js?hfrom_handle=639692&";
-const STREAM = "/ccm/ccm_play.js?hfrom_handle=%FROM_HANDLE%&hqid=%QID%&dsess=1&dsess_nid=%NID%&dsess_sn=%USERNAME%&dsetup=1&dsetup_stream=RTP%5fUnicast&dsetup_trans=1&dsetup_trans_proto=rtsp&dtoken=p2";
-var md5_ex = {
-  hex: function (val) { 
-    return crypto.createHash('md5').update(val,'latin1','latin1').digest('hex');
+let camera = null;
+// Base url
+app.get('/live', function (req, res) {
+  if (camera == null) {
+    res.send('No camera defined');
+    return;
+  } else {
+    camera.createSnapshot().then( response => {
+       res.writeHead(200, {'Content-Type': 'image/jpeg' });
+       res.end(response, 'binary');
+    }).catch(error => {
+      res.send(error);
+    });
   }
-};
+});
 
-function parseMessage(msg) {
-	var tmp = msg.replace("message(","");
-	tmp = tmp.substr(0, tmp.length - 2);
-	return jsonic(tmp);
-}
-const priv = mdh.gen_private();
-const pub = mdh.gen_public(priv);
-
-function getInfo() {
-  const url = IP_CAMERA + INFO;
-  console.log(url);
-  var deferred = Q.defer();
-  axios
-    .get(url)
-    .then(response => {
-      const resp = parseMessage(response.data);
-      console.log(resp);
-      deferred.resolve(resp);
-  }).catch(error => {
-    deferred.reject(error);
-  });  
-  return deferred.promise;
-}
-
-function subscribe(resp, sid, qid ,shared_key) {
-  resp.to_handle++;
-  console.log("qid="+qid+" shared_key="+shared_key);
-  var nid = mcodec.nid(resp.to_handle, sid, shared_key, 0, null, null, md5_ex, "hex");
-  const url = IP_CAMERA + SUBSCRIBE.replace("%FROM_HANDLE%",resp.to_handle).replace("%QID%",qid).replace("%NID%", nid);
-  console.log(url);
-  var deferred = Q.defer();
-  axios
-    .get(url)
-    .then(response => {
-      const resp = parseMessage(response.data);
-      console.log(resp);
-      deferred.resolve(resp);
-  }).catch(error => {
-    deferred.reject(error);
-  });  
-  return deferred.promise;
-}
-
-function stream(resp, user, sid, qid ,shared_key) {
-  resp.to_handle++;
-  var nid = mcodec.nid(resp.to_handle, sid, shared_key, 0, null, null, md5_ex, "hex");
-  const url = IP_CAMERA + STREAM.replace("%FROM_HANDLE%",resp.to_handle).replace("%QID%",qid).replace("%NID%", nid).replace("%USERNAME%",user);
-  console.log(url);
-  var deferred = Q.defer();
-  axios
-    .get(url)
-    .then(response => {
-      const resp = parseMessage(response.data);
-      console.log(resp);
-      deferred.resolve(resp);
-  }).catch(error => {
-    deferred.reject(error);
-  });  
-  return deferred.promise;
-}
-
-
-function createMmq(resp) {
-  resp.to_handle++;
-  const url = IP_CAMERA + MMQ_CREATE.replace("%FROM_HANDLE%",resp.to_handle);
-  console.log(url);
-  var deferred = Q.defer();
-  axios
-    .get(url)
-    .then(response => {
-      const resp = parseMessage(response.data);
-      console.log(resp);
-      deferred.resolve(resp);
-  }).catch(error => {
-    deferred.reject(error);
-  });  
-  return deferred.promise;
-}
-
-function pickMmq(resp, qid) {
-  resp.to_handle++;
-  const url = IP_CAMERA + MMQ_PICK.replace("%FROM_HANDLE%",resp.to_handle).replace("%QID%",qid);
-  console.log(url);
-  var deferred = Q.defer();
-  axios
-    .get(url)
-    .then(response => {
-      const resp = parseMessage(response.data);
-      console.log(resp);
-      deferred.resolve(resp);
-  }).catch(error => {
-    deferred.reject(error);
-  });  
-  return deferred.promise;
-}
-
-function logout(from_handle, qid, sid, shared_key) {
-  from_handle++;
-  var nid = mcodec.nid(from_handle, sid, shared_key, 0, null, null, md5_ex, "hex");
-  const url = IP_CAMERA + LOGOUT.replace("%FROM_HANDLE%",from_handle).replace("%QID%",qid).replace("%NID%", nid.replace(new RegExp('\\.', 'g'),"%"));
-  console.log(url);
-  var deferred = Q.defer();
-  axios
-    .get(url)
-    .then(response => {
-      const resp = parseMessage(response.data);
-      console.log(resp);
-      deferred.resolve(resp);
-  }).catch(error => {
-    deferred.reject(error);
-  });  
-  return deferred.promise;
-}
-
-
-function createImageConf(resp, sid, user, shared_key) {
-  resp.to_handle++;
-  console.log("shared_key="+shared_key+" sid="+sid);
-  var nid = mcodec.nid(resp.to_handle, sid, shared_key, 0, null, null, md5_ex, "hex");
-  const imgUrl = IP_CAMERA + IMG_GET.replace("%FROM_HANDLE%",resp.to_handle).replace("%NID%", nid).replace("%USERNAME%",user);
-  console.log(imgUrl);
-  var deferred = Q.defer();
-  axios
-    .get(imgUrl)
-    .then(response => {
-      const resp = parseMessage(response.data);
-      console.log(resp);
-      deferred.resolve(resp);
-  }).catch(error => {
-    deferred.reject(error);
-  });  
-  return deferred.promise;
-}
-
-
-function createSnapshot(resp, sid, user, shared_key) {
-  resp.to_handle++;
-  var nid = mcodec.nid(resp.to_handle, sid, shared_key, 0, null, null, md5_ex, "hex");
-  const imgUrl = IP_CAMERA + PIC_GET.replace("%NID%", nid).replace("%USERNAME%",user);
-  console.log(imgUrl);
-  var deferred = Q.defer();
-  axios
-    .get(imgUrl, { responseType: 'arraybuffer', headers: {
-      'Accept': 'image/jpeg',
-    }})
-    .then(response => {
-      fs.writeFile("test.jpg", response.data,  "binary",function(err) {
-          if(err) {
-              console.log(err);
-          } else {
-              console.log("The file was saved!");
-          }
-      });
-      deferred.resolve(response.data);
-  }).catch(error => {
-    deferred.reject(error);
-  });  
-  return deferred.promise;
-}
-
-function createPublicKey() {
-  var deferred = Q.defer();
-  axios
-    .get(IP_CAMERA + GENERATE_DH.replace("%KEY_2AB%",pub))
-    .then(response => {
-      var obj = parseMessage(response.data);
-      console.log(IP_CAMERA + GENERATE_DH.replace("%KEY_2AB%",pub),obj);
-      const shared_key = mdh.gen_shared_secret(priv,obj.data.key_b2a);
-      deferred.resolve({to_handle: obj.to_handle, shared_key : shared_key, lid:  obj.data.lid});
-  }).catch(error => {
-      deferred.reject(error);
+app.listen(3000, function () {
+  camera = new Camera("http","192.168.2.73","80");
+  camera.initialize();
+  console.log('Starting camera bridge!')
+  // Start program
+  camera.getInfo().then( response => {
+    console.log(response);
+    return camera.generateSharedKey();
+  }).then( publicReponse => {
+    console.log(publicReponse);
+    return camera.login( "Snoopy20");
+  }).then( loginResponse => {
+    console.log(loginResponse);
+    return camera.createMmq();
+  }).then( mmqResponse => {
+    console.log(mmqResponse);
+    return camera.subscribe();
+  }).then( subscribeResponse => {
+    console.log(subscribeResponse);
+    return camera.getImageConf();
   });
-  return deferred.promise;
-}
-
-function login(obj, user, password) {
-  obj.to_handle++;
-  console.log(obj);
-  var deferred = Q.defer();
-  var nid = mcodec.nid(obj.to_handle, obj.lid, obj.shared_key, 2, null, null, md5_ex, "hex");
-  console.log("nid="+nid+" shared_key="+obj.shared_key);
-  var t = CryptoJS.MD5(password); 
-  var c = CryptoJS.MD5(obj.shared_key),
-      d = CryptoJS.enc.Hex.parse("0000000000000000");
-  var enc_pass = CryptoJS.DES.encrypt(t, c, {
-        iv: d,
-        padding: CryptoJS.pad.NoPadding
-    }).ciphertext.toString();
-  const loginUrl = IP_CAMERA + LOGIN.replace("%FROM_HANDLE%",obj.to_handle).replace("%USERNAME%",user).replace("%LID%",obj.lid).replace("%NID%", nid).replace("%PASSWORD%",enc_pass);
-  axios
-    .get(loginUrl)
-    .then(response => {
-      var resp = parseMessage(response.data);
-      console.log(loginUrl+" => ",resp);
-      deferred.resolve(resp);
-    })
-    .catch(error => {
-      deferred.reject(error);
-    });  
-  return deferred.promise;
-}
-
-
-var shared_key = null;
-var sid = 0;
-var lid = 0;
-var qid = 0;
-var username = "";
-var from_handle = 0;
-// Start program
-getInfo().then( response => {
-  username = response.data.sn;
-  return createPublicKey();
-}).then( publicReponse => {
-  shared_key = publicReponse.shared_key;
-  lid = publicReponse.lid;
-  return login(publicReponse, username, "Snoopy20");
-}).then( loginResponse => {
-  sid = loginResponse.data.sid;
-  return createMmq(loginResponse);
-}).then( mmqResponse => {
-  qid = mmqResponse.data.qid;
-  return subscribe(mmqResponse, sid, mmqResponse.data.qid, shared_key);
-}).then( subscribeResponse => {
-  return createImageConf(subscribeResponse, sid, username, shared_key);
-}).then( imageResponse => {
-  from_handle = imageResponse.to_handle;
-  return stream(imageResponse, username, sid, qid ,shared_key)
-  // return createSnapshot(imageResponse, sid, username, shared_key);
-})
-//.then( snapshotResponse => {
-//   logout(from_handle, qid, sid, username, shared_key);
-// })
-.catch( error => {
-   console.log(error);
 });
---
+++ mipc/camera.js
@@ -0,0 +1,283 @@
+class Camera {
+
+    constructor(protocol, cameraIP, cameraPort) {
+      this.Q = require('q');
+      this.axios = require("axios");
+      this.jsonic = require("jsonic");
+      this.CryptoJS = require("crypto-js");
+      this.fs = require('fs');
+      this.crypto = require("crypto");
+      this.mdh = require("./deps/mdh.js");
+      this.mcodec = require("./deps/mcodec.js");
+
+      this.IP_CAMERA = protocol+"://"+cameraIP+":"+cameraPort;
+      this.GENERATE_DH = "/ccm/cacs_dh_req.js?hfrom_handle=%FROM_HANDLE%&dbnum_prime="+this.mdh.prime+"&droot_num="+this.mdh.g+"&dkey_a2b=%KEY_2AB%&dtid=0";
+      this.LOGIN = "/ccm/cacs_login_req.js?hfrom_handle=%FROM_HANDLE%&dlid=%LID%&dnid=%NID%&duser=%USERNAME%&dpass=%PASSWORD%&dsession_req=1&dparam__x_countz_=1&dparam=1&dparam_name=spv&dparam_value=v1";
+      this.LOGOUT = "/ccm/cacs_logout_req.js?hfrom_handle=%FROM_HANDLE%&hqid=%QID%&dnid=%NID%";
+      this.PIC_GET = "/ccm/ccm_pic_get.jpg?hfrom_handle=887330&dsess=1&dsess_nid=%NID%&dsess_sn=%USERNAME%&dtoken=p0_xxxxxxxxxx";
+      this.IMG_GET = "/ccm/ccm_img_get.js?hfrom_handle=%FROM_HANDLE%&dsess=1&dsess_nid=%NID%&dsess_sn=%USERNAME%&dtoken=vs0";
+      this.MMQ_CREATE = "/ccm/mmq_create.js?hfrom_handle=%FROM_HANDLE%&dtimeout=30000";
+      this.MMQ_PICK = "/ccm/mmq_pick.js?hfrom_handle=%FROM_HANDLE%&hqid=%QID%&dqid=%QID%&dtimeout=300000";
+      this.SUBSCRIBE = "/ccm/ccm_subscribe.js?hfrom_handle=%FROM_HANDLE%&hqid=%QID%&dsess=1&dsess_nid=%NID%";
+      this.INFO = "/ccm/ccm_info_get.js?hfrom_handle=639692&";
+      this.STREAM = "/ccm/ccm_play.js?hfrom_handle=%FROM_HANDLE%&hqid=%QID%&dsess=1&dsess_nid=%NID%&dsess_sn=%USERNAME%&dsetup=1&dsetup_stream=RTP%5fUnicast&dsetup_trans=1&dsetup_trans_proto=rtsp&dtoken=p2";
+      this.privateKey = this.mdh.gen_private();
+      this.publicKey = this.mdh.gen_public(this.privateKey);
+      this.shared_key = null;
+      this.md5_ex = {
+        crypto : this.crypto,
+        hex: function (val) { 
+          return this.crypto.createHash('md5').update(val,'latin1','latin1').digest('hex');
+        }
+      };
+      this.handle = 0;
+      this.info = null;
+      this.id = {
+        sid :' 0x0',
+        qid : 0,
+        lid : '0x0'
+      };
+      this.isConnected = false;
+    }
+
+    isConnected() {
+      return this.isConnected;
+    }
+
+    initialize() {
+      this.handle = 705790;
+    }
+
+    nextHandle() {
+      this.handle++;
+    }
+
+    generateNID(data, type) {
+      return this.mcodec.nid(this.handle, data, this.shared_key, type, null, null, this.md5_ex, "hex");
+    }
+
+    parseMessage(msg) {
+    	let tmp = msg.replace("message(","");
+    	tmp = tmp.substr(0, tmp.length - 2);
+    	return this.jsonic(tmp);
+    }
+
+    generateUrl(type) {
+      this.nextHandle();
+      let url = this.IP_CAMERA;
+      let nid = "";
+      switch(type) {
+        case "INFO" :
+          url += this.INFO.replace("%FROM_HANDLE%",this.handle);
+          break;
+        case "LOGIN":
+          nid = this.generateNID(this.id.lid, 2);
+          url += this.LOGIN.replace("%FROM_HANDLE%",this.handle).replace("%USERNAME%",this.info.sn).replace("%LID%",this.id.lid).replace("%NID%", nid);
+          break;
+        case "LOGOUT":
+          nid = this.generateNID(this.id.sid, 0);
+          url += this.LOGOUT.replace("%FROM_HANDLE%",this.handle).replace("%QID%",this.id.qid).replace("%NID%", nid);
+          break;
+        case "GENERATE_DH":
+          url += this.GENERATE_DH.replace("%FROM_HANDLE%",this.handle).replace("%KEY_2AB%",this.publicKey);
+          break;
+        case "SUBSCRIBE":
+          nid = this.generateNID(this.id.sid, 0);
+          url += this.SUBSCRIBE.replace("%FROM_HANDLE%",this.handle).replace("%QID%",this.id.qid).replace("%NID%", nid);
+          break;
+        case "STREAM":
+          nid = this.generateNID(this.id.sid, 0);
+          url += this.STREAM.replace("%FROM_HANDLE%",this.handle).replace("%QID%",this.id.qid).replace("%NID%", nid).replace("%USERNAME%",this.info.sn);
+          break;
+        case "MMQ_CREATE":
+          url += this.MMQ_CREATE.replace("%FROM_HANDLE%",this.handle);
+          break;
+        case "MMQ_PICK":
+          url += this.MMQ_PICK.replace("%FROM_HANDLE%",this.handle).replace("%QID%",this.id.qid);
+          break;
+        case "GET_IMG_CONF":
+          nid = this.generateNID(this.id.sid, 0);
+          url += this.IMG_GET.replace("%FROM_HANDLE%",this.handle).replace("%NID%", nid).replace("%USERNAME%",this.info.sn);
+          break;
+        case "CREATE_SNAPSHOT":
+          nid = this.generateNID(this.id.sid, 0);
+          url += this.PIC_GET.replace("%NID%", nid).replace("%USERNAME%",this.info.sn);
+          break;
+      }
+      console.log(url);
+      return url;
+    }
+
+    getInfo() {
+      if (this.info == null) {
+        const url = this.generateUrl("INFO");
+        const deferred = this.Q.defer();
+        this.axios
+          .get(url)
+          .then(response => {
+            const resp = this.parseMessage(response.data);
+            this.info = resp.data;
+            deferred.resolve(resp);
+        }).catch(error => {
+          deferred.reject(error);
+        });  
+        return deferred.promise;
+      } else {
+        return this.info;
+      }
+    }
+
+    subscribe() {
+      const url = this.generateUrl("SUBSCRIBE");
+      const deferred = this.Q.defer();
+      this.axios
+        .get(url)
+        .then(response => {
+          const resp = this.parseMessage(response.data);
+          deferred.resolve(resp);
+      }).catch(error => {
+        deferred.reject(error);
+      });  
+      return deferred.promise;
+    }
+
+    stream() {
+      const url = this.generateUrl("STREAM");
+      const deferred = this.Q.defer();
+      this.axios
+        .get(url)
+        .then(response => {
+          const resp = this.parseMessage(response.data);
+          deferred.resolve(resp);
+      }).catch(error => {
+        deferred.reject(error);
+      });  
+      return deferred.promise;
+    }
+
+
+    createMmq() {
+      const url = this.generateUrl("MMQ_CREATE");
+      const deferred = this.Q.defer();
+      this.axios
+        .get(url)
+        .then(response => {
+          const resp = this.parseMessage(response.data);
+          this.id.qid = resp.data.qid;
+          deferred.resolve(resp);
+      }).catch(error => {
+        deferred.reject(error);
+      });  
+      return deferred.promise;
+    }
+
+    pickMmq() {
+      const url = this.generateUrl("MMQ_PICK");
+      const deferred = Q.defer();
+      this.axios
+        .get(url)
+        .then(response => {
+          const resp = this.parseMessage(response.data);
+          deferred.resolve(resp);
+      }).catch(error => {
+        deferred.reject(error);
+      });  
+      return deferred.promise;
+    }
+
+    logout() {
+      const url = this.generateUrl("LOGOUT");
+      const deferred = this.Q.defer();
+      this.axios
+        .get(url)
+        .then(response => {
+          const resp = this.parseMessage(response.data);
+          this.isConnected = true;
+          deferred.resolve(resp);
+      }).catch(error => {
+        deferred.reject(error);
+      });  
+      return deferred.promise;
+    }
+
+
+    getImageConf() {
+      const imgUrl = this.generateUrl("GET_IMG_CONF");
+      const deferred = this.Q.defer();
+      this.axios
+        .get(imgUrl)
+        .then(response => {
+          const resp = this.parseMessage(response.data);
+          deferred.resolve(resp);
+      }).catch(error => {
+        deferred.reject(error);
+      });  
+      return deferred.promise;
+    }
+
+
+    createSnapshot() {
+      const imgUrl = this.generateUrl("CREATE_SNAPSHOT");
+      const deferred = this.Q.defer();
+      this.axios
+        .get(imgUrl, { responseType: 'arraybuffer', headers: {
+          'Accept': 'image/jpeg',
+        }})
+        .then(response => {
+/*          this.fs.writeFile("test.jpg", response.data,  "binary",function(err) {
+              if(err) {
+                  console.log(err);
+              } else {
+                  console.log("The file was saved!");
+              }
+          });
+*/
+          deferred.resolve(response.data);
+      }).catch(error => {
+        deferred.reject(error);
+      });  
+      return deferred.promise;
+    }
+
+    generateSharedKey() {
+      const deferred = this.Q.defer();
+      this.axios
+        .get(this.generateUrl("GENERATE_DH"))
+        .then(response => {
+          const obj = this.parseMessage(response.data);
+          this.shared_key = this.mdh.gen_shared_secret(this.privateKey,obj.data.key_b2a);
+          this.id.lid = obj.data.lid;
+          deferred.resolve(obj);
+      }).catch(error => {
+          deferred.reject(error);
+      });
+      return deferred.promise;
+    }
+
+    login(password) {
+      const deferred = this.Q.defer();
+      const md5_pass = this.CryptoJS.MD5(password); 
+      const md5_key = this.CryptoJS.MD5(this.shared_key);
+      const iv_init = this.CryptoJS.enc.Hex.parse("0000000000000000");
+
+      const enc_pass = this.CryptoJS.DES.encrypt(md5_pass, md5_key, {
+            iv: iv_init,
+            padding: this.CryptoJS.pad.NoPadding
+        }).ciphertext.toString();
+      const loginUrl = this.generateUrl("LOGIN").replace("%PASSWORD%",enc_pass);
+      this.axios
+        .get(loginUrl)
+        .then(response => {
+          const resp = this.parseMessage(response.data);
+          this.id.sid = resp.data.sid;
+          this.isConnected = true;
+          deferred.resolve(resp);
+        })
+        .catch(error => {
+          deferred.reject(error);
+        });  
+      return deferred.promise;
+    }
+}
+
+module.exports = Camera;
